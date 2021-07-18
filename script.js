fd9ee901cdcad79b2d48b0baf0767f5532f61479
Sands <82897602+Sands-45@users.noreply.github.com>
2021-07-18 21:12:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ class TypeWriter {
   constructor(txtElement, words, wait = 3000) {
     this.txtElement = txtElement;
     this.words = words;
-    this.txt = '';
+    this.txt = "";
     this.wordIndex = 0;
     this.wait = parseInt(wait, 10);
     this.type();
@@ -17,7 +17,7 @@ class TypeWriter {
     const fullTxt = this.words[current];
 
     // Check if deleting
-    if(this.isDeleting) {
+    if (this.isDeleting) {
       // Remove char
       this.txt = fullTxt.substring(0, this.txt.length - 1);
     } else {
@@ -31,17 +31,17 @@ class TypeWriter {
     // Initial Type Speed
     let typeSpeed = 300;
 
-    if(this.isDeleting) {
+    if (this.isDeleting) {
       typeSpeed /= 2;
     }
 
     // If word is complete
-    if(!this.isDeleting && this.txt === fullTxt) {
+    if (!this.isDeleting && this.txt === fullTxt) {
       // Make pause at end
       typeSpeed = this.wait;
       // Set delete to true
       this.isDeleting = true;
-    } else if(this.isDeleting && this.txt === '') {
+    } else if (this.isDeleting && this.txt === "") {
       this.isDeleting = false;
       // Move to next word
       this.wordIndex++;
@@ -53,140 +53,79 @@ class TypeWriter {
   }
 }
 
-
 // Init On DOM Load
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 // Init App
 function init() {
-  const txtElement = document.querySelector('.txt-type');
-  const words = JSON.parse(txtElement.getAttribute('data-words'));
-  const wait = txtElement.getAttribute('data-wait');
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
   // Init TypeWriter
   new TypeWriter(txtElement, words, wait);
 }
 
-
 //--------------Charts
- var optionsW = {
-   series: [44, 55, 67, 83],
-   chart: {
-     height: 350,
-     type: "radialBar",
-   },
-   plotOptions: {
-     radialBar: {
-       dataLabels: {
-         name: {
-           fontSize: "22px",
-         },
-         value: {
-           fontSize: "16px",
-         },
-         total: {
-           show: true,
-           label: "Total",
-           formatter: function (w) {
-             // By default this function returns the average of all series. The below is just an example to show the use of custom formatter function
-             return 249;
-           },
-         },
-       },
-     },
-   },
-   labels: ["Apples", "Oranges", "Bananas", "Berries"],
- };
-
- var rchart = new ApexCharts(document.getElementById("radiusChart"), optionsW);
- rchart.render();
-
-//----------------------------------      
 var options = {
-  series: [
-    {
-      name: "TEAM A",
-      type: "column",
-      data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
-    },
-    {
-      name: "TEAM B",
-      type: "area",
-      data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
-    },
-    {
-      name: "TEAM C",
-      type: "line",
-      data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
-    },
-  ],
+  series: [76, 67, 61, 90],
   chart: {
-    height: 350,
-    type: "line",
-    stacked: false,
-  },
-  stroke: {
-    width: [0, 2, 5],
-    curve: "smooth",
+    height: 390,
+    type: "radialBar",
   },
   plotOptions: {
-    bar: {
-      columnWidth: "50%",
-    },
-  },
-
-  fill: {
-    opacity: [0.85, 0.25, 1],
-    gradient: {
-      inverseColors: false,
-      shade: "light",
-      type: "vertical",
-      opacityFrom: 0.85,
-      opacityTo: 0.55,
-      stops: [0, 100, 100, 100],
-    },
-  },
-  labels: [
-    "01/01/2021",
-    "02/01/2021",
-    "03/01/2021",
-    "04/01/2021",
-    "05/01/2021",
-    "06/01/2021",
-    "07/01/2021",
-    "08/01/2021",
-    "09/01/2021",
-    "10/01/2021",
-    "11/01/2021",
-  ],
-  markers: {
-    size: 0,
-  },
-  xaxis: {
-    type: "datetime",
-  },
-  yaxis: {
-    title: {
-      text: "Points",
-    },
-    min: 0,
-  },
-  tooltip: {
-    shared: true,
-    intersect: false,
-    y: {
-      formatter: function (y) {
-        if (typeof y !== "undefined") {
-          return y.toFixed(0) + " points";
-        }
-        return y;
+    radialBar: {
+      offsetY: 0,
+      startAngle: 0,
+      endAngle: 270,
+      hollow: {
+        margin: 5,
+        size: "30%",
+        background: "transparent",
+        image: undefined,
+      },
+      dataLabels: {
+        name: {
+          show: false,
+        },
+        value: {
+          show: false,
+        },
       },
     },
   },
+  colors: ["#1ab7ea", "#0084ff", "#000", "#0077B5"],
+  labels: ["Calls", "Trans", "Order-Rate", "Talk-Time"],
+  legend: {
+    show: true,
+    floating: true,
+    fontSize: "16px",
+    position: "left",
+    offsetX: 160,
+    offsetY: 15,
+    labels: {
+      useSeriesColors: true,
+    },
+    markers: {
+      size: 0,
+    },
+    formatter: function (seriesName, opts) {
+      return seriesName + ":  " + opts.w.globals.series[opts.seriesIndex];
+    },
+    itemMargin: {
+      vertical: 3,
+    },
+  },
+  responsive: [
+    {
+      breakpoint: 480,
+      options: {
+        legend: {
+          show: false,
+        },
+      },
+    },
+  ],
 };
 
 var chart = new ApexCharts(document.querySelector("#Chart"), options);
 chart.render();
-      
-      
-    
- 
